7987e7c6a38d228717ab0d6001dfa0eb320046a2
Jacob J <96319709+night780@users.noreply.github.com>
2023-03-25 10:29:59 -0700

    Update EyeBreakGUI.java

--- EyeBreakGUI.java
+++ EyeBreakGUI.java
@@ -1,21 +1,23 @@
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.File;
-import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.Clip;
 import javax.swing.*;
 
+
 /**
- * The type Eye break gui.
+ * It creates a GUI with a start and stop button, a progress bar, and a label. When the start button is pressed, the
+ * progress bar starts filling up. When the progress bar is full, a dialog box pops up telling the user to take a break
+ *
+ * @author Jacob Jonas
+ * @version 2.0
+ * @date 3-25-23
  */
 public class EyeBreakGUI {
+    public static int MILLIS;
     private JButton startButton, stopButton;
     private JLabel instructionLabel;
     private JProgressBar progressBar;
     private boolean running = false;
-
     /**
      * Instantiates a new Eye break gui.
      */
@@ -23,15 +25,22 @@ public class EyeBreakGUI {
         createGUI();
     }
 
+    /**
+     * It creates a GUI with a start and stop button, a progress bar, and a label that displays instructions
+     */
     private void createGUI() {
         JFrame frame = new JFrame("Eye Break Program");
         frame.setIconImage(new ImageIcon(getClass().getResource("icon.png")).getImage());
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+
+
+
+
         instructionLabel = new JLabel("Click start to begin the eye break program");
         instructionLabel.setHorizontalAlignment(SwingConstants.CENTER);
         frame.add(instructionLabel, BorderLayout.NORTH);
-
+        addMinuteField(frame);
         JPanel panel = new JPanel(new GridLayout(2, 1));
         startButton = new JButton("Start");
         startButton.addActionListener(new ActionListener() {
@@ -52,6 +61,7 @@ public class EyeBreakGUI {
         stopButton.setEnabled(false);
         stopButton.addActionListener(new ActionListener() {
             @Override
+            // An action listener that is called when the start button is pressed.
             public void actionPerformed(ActionEvent e) {
                 if (running) {
                     running = false;
@@ -59,9 +69,13 @@ public class EyeBreakGUI {
                     stopButton.setEnabled(false);
                     instructionLabel.setText("Click start to begin the eye break program");
                     progressBar.setValue(0);
+
                 }
             }
+
         });
+        
+        
         panel.add(stopButton);
 
         progressBar = new JProgressBar();
@@ -74,6 +88,41 @@ public class EyeBreakGUI {
         frame.setVisible(true);
     }
 
+    private void addMinuteField(JFrame frame) {
+        JPanel panel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        JLabel minuteLabel = new JLabel("Minutes:");
+        panel.add(minuteLabel);
+
+        JTextField minuteField = new JTextField(2);
+        minuteField.setText("20"); // default value
+        panel.add(minuteField);
+
+        JButton setButton = new JButton("Set");
+        setButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String text = minuteField.getText();
+                try {
+                    int minutes = Integer.parseInt(text);
+                    MILLIS = minutes * 1000; // update MILLIS field with new value in milliseconds
+                    JOptionPane.showMessageDialog(null, "Eye break duration has been set to " + minutes + " minutes.", "Eye Break Duration Set", JOptionPane.INFORMATION_MESSAGE);
+                } catch (NumberFormatException ex) {
+                    JOptionPane.showMessageDialog(null, "Please enter a valid number.", "Error", JOptionPane.ERROR_MESSAGE);
+                }
+            }
+        });
+
+        panel.add(setButton);
+
+        frame.add(panel, BorderLayout.EAST);
+
+    }
+
+    /**
+     * It creates a new thread that runs a while loop that increments a progress bar until it reaches 100, then it displays
+     * a dialog box that tells the user to take an eye break, then it resets the progress bar to 0 and starts the loop over
+     * again
+     */
     private void startEyeBreakProgram() {
         new Thread(new Runnable() {
             @Override
@@ -82,19 +131,18 @@ public class EyeBreakGUI {
                     while (running) {
                         for (int i = 0; i <= 100 && running; i++) {
                             progressBar.setValue(i);
-                            Thread.sleep(1200*100); // 20 minutes
+                            Thread.sleep(MILLIS); // 20 minutes
                         }
                         if (running) {
+                            Toolkit.getDefaultToolkit().beep();
                             String message = "Time to take an eye break! Look away from the screen for 10-20 seconds.";
                             String title = "Eye Break";
                             int messageType = JOptionPane.INFORMATION_MESSAGE;
                             JOptionPane pane = new JOptionPane(message, messageType, JOptionPane.DEFAULT_OPTION, null, new Object[] {}, null);
                             JDialog dialog = pane.createDialog(title);
-                            //((JDialog) dialog).setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
                             dialog.setModal(true);
                             dialog.setAlwaysOnTop(true);
                             dialog.setVisible(true);
-
                             progressBar.setValue(0);
                         }
                     }
@@ -109,6 +157,9 @@ public class EyeBreakGUI {
             }
         }).start();
     }
+
+
+
 
     /**
      * The entry point of application.
